2ddd9c54a9ee16132bb5b51f0c345abfb09b31b5
Jean Delefrati <jeandelefrati@gmail.com>
2023-01-02 02:02:43 -0300

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get :planets, to: "planets#index"
-  get "planets/{id}", to: "planets#show"
-  delete "planets/{id}", to: "planets#destroy"
+  get "planets/:id", to: "planets#show"
+  delete "planets/:id", to: "planets#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
